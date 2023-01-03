566e2f592cf99a83ae978bf21aeb660e731560f2
Aditya Boddepallli <adityaboddepalli0@gmail.com>
2023-01-02 22:20:19 -0400

    Initial Commit

---
+++ Scrumpad/ScrumpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumpadApp.swift
+//  Scrumpad
+//
+//  Created by Aditya Boddepalli on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
